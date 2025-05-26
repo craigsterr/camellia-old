9b0d3e391ef90fe1a87637ef6cf4661ef1924fc1
craig <craiglo2@illinois.edu>
2025-05-26 17:23:07 -0500

    change prefix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/camellia",
-  assetPrefix: "/camellia/",
+  // basePath: "/camellia",
+  // assetPrefix: "/camellia/",
   images: {
     remotePatterns: [
       {
